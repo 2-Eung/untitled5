770b15eec22882a3917b2f565f5fadbba3522b73
2-Eung <eung4256@gmail.com>
2025-06-09 10:16:35 +0900

    overloading 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
     public static void main(String[] args) {
-
+        System.out.println("a + b = " + add(1, 2));
+        System.out.println("a + b +c = " + add(1, 2, 3));
     }
 }
