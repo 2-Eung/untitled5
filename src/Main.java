d226f0ff83481e4d5f79f1d640270fea73e361b9
2-Eung <eung4256@gmail.com>
2025-06-09 13:41:11 +0900

    Array 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
 public class Main {
+    public static int sums(int[] a) { // 굳이 함수로 할 필요는 없다 있다면 2,3,4,5,배수로 n개의 숫자를 출력한다 라고하면
+        int dd = 0;                   // 매개변수로 배수의 값과 n 을 받아서 함수형식으로 만들 수 있다.
+        for (int i = 0; i < a.length; i++) {
+            dd += a[i];
+        };
+        return dd;
+    }
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
-        System.out.println(Arrays.toString(numbers));
-        for (int i = 0; i < 5; i++) {
-            System.out.println(numbers[i]);
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        int[] numbers = {2, 4, 6, 8, 10};
+        int sum = sums(numbers);
+        System.out.println(sum);
     }
 }
