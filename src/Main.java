df09d8461402f9cb36e7366a6770dc36288e9f34
2-Eung <eung4256@gmail.com>
2025-06-09 10:24:13 +0900

    overloading 2 ex)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
+    public static void greet() {
+        System.out.println("Hello, World!");
     }
-    public static int add(int a, int b, int c) {
-        return a + b + c;
+    public static void greet (String a) {
+        System.out.println("Hello, " + a);
     }
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(1, 2));
-        System.out.println("a + b +c = " + add(1, 2, 3));
+        greet();
+        greet("Alice!");
     }
 }
